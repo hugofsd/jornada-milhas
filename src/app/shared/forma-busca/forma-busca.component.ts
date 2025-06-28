8c988ca6f60a1cca538459cdcdaa38650b3ee94b
hugofsd <hugo619@outlook.com>
2025-06-28 14:54:00 -0300

    Criacao de modal

--- src/app/shared/forma-busca/forma-busca.component.ts
+++ src/app/shared/forma-busca/forma-busca.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { ModalComponent } from '../modal/modal.component';
 
 @Component({
   selector: 'app-forma-busca',
   templateUrl: './forma-busca.component.html',
-  styleUrls: ['./forma-busca.component.scss']
+  styleUrls: ['./forma-busca.component.scss'],
 })
 export class FormaBuscaComponent {
+  constructor(public dialog: MatDialog) {}
 
+  openDialog() {
+    this.dialog.open(ModalComponent, {
+      width: '50%',
+    });
+  }
 }
