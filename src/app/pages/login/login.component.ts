a9459d5bd6b45458a36d23876171083aad33bbdc
hugofsd <hugo619@outlook.com>
2025-07-09 20:43:17 -0300

    Serviço de autenticação e validação de login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AutenticacaoService } from 'src/app/core/services/autenticacao.service';
 
 @Component({
   selector: 'app-login',
@@ -9,18 +11,33 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 export class LoginComponent implements OnInit {
   loginForm!: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) {}
+  constructor(
+    private formBuilder: FormBuilder,
+    private authService: AutenticacaoService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
     this.loginForm = this.formBuilder.group({
-      email: [null],
-      senha: [null],
+      email: [null, [Validators.required, Validators.email]],
+      senha: [null, Validators.required],
     });
   }
 
   login() {
-    const email = this.loginForm.value.email;
-    const senha = this.loginForm.value.senha;
-    console.log('Login realizado com sucesso', this.loginForm.value);
+    if (this.loginForm.valid) {
+      const email = this.loginForm.value.email;
+      const senha = this.loginForm.value.senha;
+      this.authService.autenticar(email, senha).subscribe({
+        next: (value) => {
+          console.log('Autenticado com sucesso', value);
+          this.router.navigateByUrl('/');
+          this.loginForm.reset();
+        },
+        error: (err) => {
+          console.log('Problema na autenticação', err);
+        },
+      });
+    }
   }
 }
