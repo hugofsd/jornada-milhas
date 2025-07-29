d9d90975ecb649dc201b70237dd6a389e965c149
hugofsd <hugo619@outlook.com>
2025-07-29 20:51:55 -0300

    editarCadastro e buscarCadastro

--- src/app/core/services/cadastro.service.ts
+++ src/app/core/services/cadastro.service.ts
@@ -1,7 +1,7 @@
-import { HttpClient } from '@angular/common/http';
+import { PessoaUsuaria } from 'src/app/core/types/type';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
-import { PessoaUsuaria } from '../types/type';
 import { Observable } from 'rxjs';
 
 @Injectable({
@@ -16,5 +16,24 @@ export class CadastroService {
       `${this.apiUrl}/auth/cadastro`,
       pessoaUsuaria
     );
+  }
+
+  buscarCadastro(token: string): Observable<PessoaUsuaria> {
+    const headers = new HttpHeaders({ Authorization: `Bearer ${token}` });
+
+    return this.http.get<PessoaUsuaria>(`${this.apiUrl}/auth/perfil`, {
+      headers,
+    });
+  }
+
+  editarCadastro(
+    pessoaUsuaria: PessoaUsuaria,
+    token: string
+  ): Observable<PessoaUsuaria> {
+    const headers = new HttpHeaders({ Authorization: `Bearer ${token}` });
+
+    return this.http.patch<PessoaUsuaria>(`${this.apiUrl}/auth/perfil`, {
+      headers,
+    });
   }
 }
