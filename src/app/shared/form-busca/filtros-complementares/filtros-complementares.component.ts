69712f53d474b5970eeb38e39bcf9369cae8782b
hugofsd <hugo619@outlook.com>
2025-08-10 12:04:03 -0300

    Iniciando com filtros

--- src/app/shared/form-busca/filtros-complementares/filtros-complementares.component.ts
+++ src/app/shared/form-busca/filtros-complementares/filtros-complementares.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { FormBuscaService } from 'src/app/core/services/form-busca.service';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-filtros-complementares',
   templateUrl: './filtros-complementares.component.html',
-  styleUrls: ['./filtros-complementares.component.scss']
+  styleUrls: ['./filtros-complementares.component.scss'],
 })
 export class FiltrosComplementaresComponent {
+  @Output() realizarBusca = new EventEmitter();
 
+  constructor(private formBuscaService: FormBuscaService) {}
+
+  busca() {
+    if (!this.formBuscaService.formEstaValido) {
+      // Marca todos os campos do formulário como "tocados" para exibir as mensagens de erro
+      this.formBuscaService.formBusca.markAllAsTouched();
+      window.scroll({
+        top: 0,
+        left: 0,
+        behavior: 'smooth',
+      });
+      return;
+    }
+    this.realizarBusca.emit(this.formBuscaService.obterDadosBusca());
+  }
 }
